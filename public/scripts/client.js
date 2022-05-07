cd4172e401e124bf6cea983d06c82b8eec1ed98d
symphony <jciurc@gmail.com>
2022-05-07 15:44:09 -0600

    create basic client js structure

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,38 +1,44 @@
-// == events ==
-// delete tweet
-$('.delete-button').on('click', (event) => {
-  event.preventDefault();
-  const $todo = $(this).closest('article');
-  const id = $todo.id;
+$(document).ready(() => {
 
-  $.delete('/' + id)
-    .then(() => {
-      $todo.closest('section').removeElement($todo)
-    })
-});
-
-const safeHtml = (string) => {
-
-}
+  // = initial page load =
+  loadTodos();
 
 
-const buildTodoCard = (todo) => {
-  const htmlString =  `
-    <div> ${safeHtml(todo.description)} </div>
-    <div> ${todo.category} </div>
-    `
-    return htmlString;
-}
+  $('.delete-button').on('click', (event) => {
+    event.preventDefault();
+    const $todo = $(this).closest('article');
+    const id = $todo.id;
 
-renderTodos = (todos) => {
-  buildTodoCard(todos[0])
-}
-
-router.get('/properties', (req, res) => {
-  db.getAllTodos(req.cookies.user)
-  .then(renderTodos)
-  .catch(e => {
-    console.error(e);
-    res.send(e)
+    $.delete('/' + id)
+      .then(() => {
+        $todo.closest('section').removeElement($todo)
+      })
   });
+
+  const safeHtml = (string) => {
+
+  }
+
+
+  const buildTodoCard = (todo) => {
+    const htmlString =  `
+      <div> ${safeHtml(todo.description)} </div>
+      <div> ${todo.category} </div>
+      `
+      return htmlString;
+  }
+
+  renderTodos = (todos) => {
+    buildTodoCard(todos[0])
+  }
+
+  const loadTodos = (id) => {
+    $.get('/todos', id)
+    .then(renderTodos)
+    .catch((err) => {
+      console.error(err);
+      res.send(err);
+    });
+  };
+
 });
