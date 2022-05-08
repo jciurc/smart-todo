33715edac9ca861913f7e00ed8deacd12f0294ba
symphony <jciurc@gmail.com>
2022-05-07 18:54:17 -0600

    set up basic api call loop

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -1,17 +1,69 @@
-const fetchDataForUser = function (url, username, callback) {
-  request(url, (error, data) => {
+$(document).ready(() => {
 
-    if (error) {
-      return callback(error, null);
+
+  $('#new-tweet').on('submit', newTodo);
+});
+
+const fetchMoviesForUser = function () {
+  const movieOptions = {
+    method: "GET",
+    url: "https://movie-database-alternative.p.rapidapi.com/",
+    params: { s: "Avengers Endgame", r: "json", page: "1" },
+    headers: {
+      "X-RapidAPI-Host": "movie-database-alternative.p.rapidapi.com",
+      "X-RapidAPI-Key": "process.env.API_KEY",
+    },
+  };
+  axios.request(movieOptions)
+    .then((response) => {
+      console.log("data", response.data);
+
+    })
+    .catch ((err) => {
+      console.log("err", err)
+  })
+};
+
+const fetchMusicForUser = function() {
+  const options = {
+    method: 'get',
+    url: 'https://shazam.p.rapidapi.com/search',
+    data: { term: "kiss the rain", locale: "en-US", offset: "0", limit: "5" },
+    headers: {
+      "X-RapidAPI-Host": "shazam.p.rapidapi.com",
+      "X-RapidAPI-Key": "process.env.API_KEY",
     }
-    const usersObject = JSON.parse(data.body).users;
-    let matchedUser = undefined;
-    for (const user in usersObject) {
-      if (user === username) {
-        matchedUser = usersObject[user];
-        
-      }
-    }
-    callback(null, matchedUser);
+  };
+
+  return $.ajax(options)
+    .then((res) => {
+      if (res.hits.tracks.length > 1) return "Music";
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+const findCategory = (text) => {
+  return Promise.any([
+    fetchMusicForUser(text),
+    fetchMoviesForUser(text)
+  ])
+  .then((category) => {
+    return category || 'Unlabeled';
+  });
+}
+
+const newTodo = (event) => {
+  event.preventDefault();
+  const $form = $(this).closest('form');
+  const $inputField = $form.find('input');
+  const description = $inputField.val() || "Avengers Endgame";
+  // error handling. text field empty
+
+  findCategory(description)
+    .then((category) => {
+    $.post('/todos', {description, category})
+    $.get('/todos');
   });
 };
