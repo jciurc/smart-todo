2ecab46cc30774b176e04c9528c76e4199fb8ff6
symphony <jciurc@gmail.com>
2022-05-08 12:16:38 -0600

    merge features/api into main

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (() => {
   $(document).ready(() => {
     // user routes
@@ -20,11 +19,6 @@
     safe.appendChild(document.createTextNode(text));
     return safe.innerHTML;
   };
-=======
-$(document).ready(() => {
-  $('.delete-button').on('click', deleteTodo);
-  // $('#new-todo').on('submit', newTodo);
->>>>>>> features/api
 
   const buildTodoCard = (todo) => {
     const htmlString = `
@@ -35,7 +29,6 @@ $(document).ready(() => {
     return htmlString;
   };
 
-<<<<<<< HEAD
   const renderTodos = (todos) => {
     const $container = $('#categories-container');
     for (const todo of todos) {
@@ -113,53 +106,3 @@ $(document).ready(() => {
   };
 
 })();
-=======
-// == helpers ==
-const safeHtml = (text) => {
-  const safe = document.createElement("div");
-  safe.appendChild(document.createTextNode(text));
-  return safe.innerHTML;
-};
-
-const buildTodoCard = (todo) => {
-  const htmlString = `
-<div class="todo rounded" ">
-  <article class="todo rounded flex" style="background-color: #225778;>
-    <p class="text-base">
-      ${safeHtml(todo.description)}
-    </p>
-  </article>
-</div>
-`;
-    return htmlString;
-}
-
-const renderTodos = (todos) => {
-  const $container = $('#categories-container');
-  for (const todo of todos) {
-    $container.find(`#${todo.name}-container`).show().prepend(buildTodoCard(todo));
-  }
-}
-
-loadTodos = () => {
-  $.get('/todos')
-  .then(renderTodos)
-};
-
-// == events ==
-const newTodo = function(event) {
-  event.preventDefault();
-  $(this).trigger('apis');
-  // error handling. text field empty
-};
-
-const deleteTodo = () => {
-  const $todo = $(this).closest('article');
-  const id = $todo.id;
-
-  $.delete('/' + id)
-    .then(() => {
-      $todo.closest('section').removeElement($todo)
-    })
-};
->>>>>>> features/api
