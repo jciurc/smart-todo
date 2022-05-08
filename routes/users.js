1bfb4104d4db1741ff554c0a9648c98f3487ea8a
Kelly Atmore <kelly.atmore47@gmail.com>
2022-05-07 23:20:41 -0700

    updates

--- routes/users.js
+++ routes/users.js
@@ -7,27 +7,43 @@
 
 const express = require('express');
 const router = express.Router();
-const { getUserByName } = require('../queries');
+const { getUserByName, getUserById } = require('../queries');
 
 router.get("/", (req, res) => {
-
+  const userId = req.cookie.user;
+  if (userId) {
+    getUserById(userId)
+      .then((user) => {
+        res.send(user || null);
+      });
+  }
+  res.send(null);
 });
 
-router.post("/", (req, res) => {
+router.post("/logout", (req,res) => {
+  res.clearCookie('user');
+  res.send(true);
+});
 
+
+router.post("/", (req, res) => {
   const name = req.body.text;
 
   getUserByName(name)
+    .then((user) => {
+      return user || addUserToDatabase();
+    })
     .then((user) => {
       res.cookie('user', user.id);
       res.json(user);
       console.log(user);
     })
+
     .catch((err) => {
       res.status(500).json({ error: err.message });
     });
 
 
-})
+});
 
 module.exports = router;
