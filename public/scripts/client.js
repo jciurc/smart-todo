f3a0b6811d02e92317bccef21d984ebf0eafb99f
Kelly Atmore <kelly.atmore47@gmail.com>
2022-05-08 19:56:50 -0700

    add category text area and checkbox

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -32,10 +32,13 @@
   const buildTodoCard = (todo) => {
     const htmlString = `
     <article class="todo rounded" alt="${todo.id}">
-      <p class="text-base rounded bg-slate-700 m-3 p-4">${safeHtml(todo.description)}</p>
+
+      <p class="text-base rounded bg-slate-700 m-3 p-4"><input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
+      ${safeHtml(todo.description)}<i class="far fa-edit cursor-pointer"></i></p>
 
 <form hidden>
 
+       <textarea name="text" class="text-base rounded bg-slate-700 m-3 p-4">${safeHtml(todo.name)}</textarea>
         <textarea name="text" class="text-base rounded bg-slate-700 m-3 p-4">${safeHtml(todo.description)}</textarea>
         <button type="submit" class="confirm-edit bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Confirm</button>
         <button type="button" class="delete-button bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete</button>
@@ -128,7 +131,7 @@
     const text = $(this).serialize()
     console.log('serialized text', text);
     //maybe second input parameter?
-    const id = $todo.attr("alt");id
+    const id = $todo.attr("alt");
 
     $.ajax({ url: "/todos/" + id, data: text, type: "PUT" })
       .then((res) => {
