ef248c64f8735a00bf6437bd7a042523cfab932e
symphony <jciurc@gmail.com>
2022-05-09 02:28:14 -0600

    todo card styling

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -34,10 +34,10 @@
 
   const buildTodoCard = (todo) => {
     const htmlString = `
-    <article class="todo rounded flex-col flex-1 justify-center" completed="${todo.completed}" alt="${todo.id}">
+    <article class="todo rounded flex-col flex-1 justify-center mb-4" completed="${todo.completed}" alt="${todo.id}">
       <p class="todo-description flex flex-1 justify-between items-center text-base ${todo.completed ? 'complete' : ''} text-center rounded bg-slate-700 m-3 p-4">
       <input ${todo.completed ? 'checked' : ''} type="checkbox" class="form-check-input hover appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" id="flexCheckDefault">
-      ${safeHtml(todo.description)}<i class="far fa-edit hover cursor-pointer"></i></p>
+      ${safeHtml(todo.description)}<i class="far fa-edit float-right hover cursor-pointer"></i></p>
 
       <form>
         <textarea name="text" class="text-base text-center rounded bg-slate-700 m-3 p-4">${safeHtml(todo.description)}</textarea>
