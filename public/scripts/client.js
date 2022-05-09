5532a2f6065a262c3aebf49d153c1c29a61a4f77
symphony <jciurc@gmail.com>
2022-05-09 11:18:08 -0600

    more card styling

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -34,7 +34,7 @@
 
   const buildTodoCard = (todo) => {
     const htmlString = `
-    <article class="todo rounded flex-col justify-center my-2" completed="${todo.completed}" alt="${todo.id}">
+    <article class="todo rounded flex-col flex-nowrap justify-center my-2" completed="${todo.completed}" alt="${todo.id}">
       <header class="card flex justify-center items-center ${todo.completed ? 'complete' : ''} rounded bg-slate-700 h-16 m-3 p-2">
         <input type="checkbox" ${todo.completed ? 'checked' : ''} class="form-check-input hover appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain cursor-pointer" id="flexCheckDefault" />
         <p class="text-base text-center self-center p-2">${safeHtml(todo.description)}</p>
@@ -43,7 +43,7 @@
 
       <form class="edit">
         <textarea name="text" class="text-base text-center self-center rounded bg-slate-700 my-2 mx-auto p-2">${safeHtml(todo.description)}</textarea>
-        <div class="relative inline-block text-left">
+        <div class="inline-block text-left">
           <button type="button" name="category" alt${todo.category_id} class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-slate-600 text-sm font-medium text-light-700 hover:bg-slate-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" id="menu-button" aria-expanded="true" aria-haspopup="true">${safeHtml(todo.name)}
             <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
