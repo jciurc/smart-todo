63b35a577122557fb9dd1009378ca9fea74be2f8
MitchScho <scho.mitch@gmail.com>
2022-05-09 23:04:55 +0000

    products api function created

--- api.js
+++ api.js
@@ -2,6 +2,28 @@ const axios = require('axios').default;
 const any = require('promise.any');
 
 // = api calls =
+const queryProducts = (text) => {
+  const options = {
+    method: "GET",
+    url: "https://amazon-price1.p.rapidapi.com/search",
+    params: { keywords: text, marketplace: "ES" },
+    headers: {
+      "X-RapidAPI-Host": "amazon-price1.p.rapidapi.com",
+      "X-RapidAPI-Key": process.env.API_KEY,
+    },
+  };
+  return axios
+    .get("https://amazon-price1.p.rapidapi.com/search", options)
+    .then((res) => {
+      console.log("products query response", res.data[0]);
+      //console.log("products query response.docs", res.data.docs[0]);
+      if (res.data.length) return "Products";
+    })
+    .catch((err) => {
+      console.error("err", err.message);
+    });
+};
+
 const queryFood = (text) => {
   const options = {
     method: "GET",
@@ -18,8 +40,8 @@ const queryFood = (text) => {
       options
     )
     .then((res) => {
-      console.log("food query response", res);
-      if (res.length > 0) return "Food";
+      console.log("food query response", res.data);
+      if (res.data.length > 0) return "Food";
     })
     .catch((err) => {
       console.error("err", err.message);
@@ -69,11 +91,13 @@ const findCategory = (text) => {
   return any([
     queryFood(text),
     queryMusic(text),
+    queryProducts(text)
   ])
     .then((category) => {
       console.log('promise any response', category);
       //return category || queryMovies(text).then(category => category || 'Unlabeled')
-      return category.then((category) => category || "Unlabeled");
+      return category
+      //.then((category) => category || "Unlabeled");
     })
     .catch((err) => {
       console.log('error finding category', err.message || '');
