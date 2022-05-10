6de3e5085845248110c50210f748faddec95778f
symphony <jciurc@gmail.com>
2022-05-09 21:44:33 -0600

    fix login screen

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -128,8 +128,6 @@
       $('#login').hide();
       $('#logout').show().find('div').append(`<p class="align-text">${safeHtml(name)}</p>`);
       $('#splash').hide();
-      $('#logout').show().find('div').append(username);
-      $('#new-todo').show().find('h1').text(`Hello, ${safeHtml(name)}!`);
       $('#new-todo').show().find('h1').text(`${greeting}, ${safeHtml(name)}!`);
       return;
     }
