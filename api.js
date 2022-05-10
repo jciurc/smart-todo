317e67f71f66dfaaf085a2fa14e09829980d694e
symphony <jciurc@gmail.com>
2022-05-10 17:20:59 -0600

    formatting

--- api.js
+++ api.js
@@ -17,20 +17,17 @@ const axiosGet = (url, host, params) => {
   .then((res) => {
     return res.data;
   })
-  .catch((err) => {
-    console.error(err.message);
-  });
 };
 
 
 const queryMusic = (text) => {
-  const url = 'https://shazam.p.rapidapi.com/search'
-  const host = 'shazam.p.rapidapi.com'
-  const params = { term: text, locale: "en-US", offset: "0", limit: "3" }
+  const url = 'https://shazam.p.rapidapi.com/search';
+  const host = 'shazam.p.rapidapi.com';
+  const params = { term: text, locale: "en-US", offset: "0", limit: "3" };
   return axiosGet(url, host, params)
     .then((data) => {
       const { title, subtitle } = data.tracks.hits[0];
-    return ['Music', 'Track' + title + 'by:' +  subtitle];
+    return 'Track' + title + 'by:' +  subtitle;
     });
 };
 
@@ -41,8 +38,8 @@ const queryBooks = (text) => {
       'X-RapidAPI-Host': 'hapi-books.p.rapidapi.com',
       'X-RapidAPI-Key': process.env.API_KEY,
     },
-
   };
+
   const url = "https://hapi-books.p.rapidapi.com/search/" + text.toLowerCase().split(" ").join("+");
   return axios.get(url, options)
     .then((res) => {
@@ -54,15 +51,10 @@ const queryBooks = (text) => {
 
     return res.data;
     })
-    .catch((err) => {
-      console.error(err.message);
-    });
 };
 
 const queryProducts = (text) => {
   const options = {
-    method: "GET",
-    url: "https://amazon-price1.p.rapidapi.com/search",
     params: { keywords: text, marketplace: "ES" },
     headers: {
       "X-RapidAPI-Host": "amazon-price1.p.rapidapi.com",
@@ -79,20 +71,12 @@ const queryProducts = (text) => {
 
     return res.data;
     })
-    .catch((err) => {
-      console.error(err.message);
-    });
 };
 
 const queryFood = (text) => {
   const options = {
-    method: "GET",
     url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/autocomplete",
     params: { query: text, number: "10" },
-    headers: {
-      "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-      "X-RapidAPI-Key": process.env.API_KEY,
-    },
   };
   return axios.get( "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/autocomplete",options)
     .then((res) => {
@@ -104,9 +88,6 @@ const queryFood = (text) => {
 
     return res.data;
     })
-    .catch((err) => {
-      console.error(err.message);
-    });
 };
 
 
@@ -129,9 +110,6 @@ const queryMovies = (text) => {
 
     return res.data;
     })
-    .catch((err) => {
-      console.error(err.message);
-    });
 };
 
 
@@ -145,27 +123,33 @@ const uclassifyRequest = (subject, text) => {
       const sorted = filtered.sort((a, b) => b[1] - a[1]);
       return sorted[0][0];
     })
+};
+
+
+// = main function =
+const findCategory = (text) => {
+  const subtopic = { Arts: 'art-topics', Home: 'home-topics' }
+  const categories = { Arts: 'art-topics', Home: 'home-topics' }
+  return uclassifyRequest('topics', text)
+    .then((topic) => {
+      return uclassifyRequest(subtopic[topic], text)
+    })
+    .then((topic) => {
+      return categories[topic];
+    })
     .catch((err) => {
-      console.log('error getting topic');
+      console.log('error getting category');
       console.error(err.message);
     });
 };
 
-const findTopic = (text) => {
-  return uclassifyRequest('topics', text)
-    .then((topic) => {
-      const subtopic = { Arts: 'art-topics', Home: 'home-topics' }
-      return uclassifyRequest(subtopic[topic], text);
-    })
-};
-
-// = main function =
-const findCategory = (text) => {
-  return findTopic(text)
-};
-
 const getSubtitle = (category, text) => {
-  // todo make run external api call
+  queries[category](text)
+
+  .catch((err) => {
+    console.log('error getting subtitle');
+    console.error(err.message);
+  });
 }
 
 module.exports = { findCategory, getSubtitle };
