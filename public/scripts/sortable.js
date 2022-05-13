784d9f199f77591762f03620758b36b6755282c0
symphony <jciurc@gmail.com>
2022-05-13 10:33:57 -0600

    drag and drop to different categories (working)

--- public/scripts/sortable.js
+++ public/scripts/sortable.js
@@ -6,22 +6,28 @@
       distance: 20, delay: 50,
       group: 'nested',
 
-      afterMove: function (placeholder, container) {
-        if(oldContainer != container){
-          if(oldContainer) oldContainer.el.removeClass("active");
+      afterMove: function(placeholder, container) {
+        if (oldContainer != container) {
+          if (oldContainer) oldContainer.el.removeClass("active");
           container.el.addClass("active");
 
           oldContainer = container;
         }
       },
 
-      onDrop: function ($item, container, _super) {
+      onDrop: function($item, container, _super) {
         container.el.removeClass("active");
         _super($item, container);
 
-        // reload all todos
-        $.post()
-        // $('main').trigger('reload');
+        // Update todo category in database on drop
+        const params = {
+          url: "/todos/" + $item.attr('alt'),
+          data: 'category_id=' + container.el[0].getAttribute('alt'),
+          type: "PUT",
+        };
+        $.ajax(params).then((todo) => {
+            $('main').trigger('reload');
+          });
       }
     });
 
